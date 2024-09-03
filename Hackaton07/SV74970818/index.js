189aa4d02907e0c28535e3748aa08a5ae14885b6
EJRuizM <u201620833@upc.edu.pe>
2024-09-02 19:02:36 -0500

    creacion de hackaton 07

---
+++ Hackaton07/SV74970818/index.js
@@ -0,0 +1,7 @@
+console.log("Hola desde node.js")
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hola que tal desde node js');
+}).listen(8080);
