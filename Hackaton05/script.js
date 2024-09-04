f85cb6658d9ec5822c7ad99481af60a11ad8b6bd
Armando David <bustamante@gmail.com>
2024-09-03 20:01:12 -0500

    agregue mi lista de imeis y numeros de serie reportados

---
+++ Hackaton05/script.js
@@ -0,0 +1,4 @@
+// Ejemplo de lista de IMEIs y números de serie reportados
+const reportedIMEIs = ["123456789012345", "987654321098765"];
+const reportedSerialNumbers = ["ABC123XYZ", "XYZ987ABC"];
+
