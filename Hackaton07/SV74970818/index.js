7edcd9d77ea422a86187b03e35dabe3848daab74
EJRuizM <u201620833@upc.edu.pe>
2024-09-04 23:48:37 -0500

    tipo de cambio peru

--- Hackaton07/SV74970818/index.js
+++ Hackaton07/SV74970818/index.js
@@ -7,27 +7,64 @@ http
     res.writeHead(200, { "Content-Type": "application/json" });
     let strURL = req.url;
 
+    // Consultar los datos de GitHub de un usuario especifico.
     if (strURL.includes("github")) {
       var q = url.parse(req.url, true).query;
-        let config = {
-          method: "get",
-          maxBodyLength: Infinity,
-          url: 'https://api.github.com/users/' + q.usuario,
-          headers: {},
-        };
-        axios
-          .request(config)
-          .then((response) => {
-            console.log(JSON.stringify(response.data));
-            res.write(JSON.stringify(response.data));
-            res.end();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: "https://api.github.com/users/" + q.usuario,
+        headers: {},
+      };
+      try {
+        const response = await axios.request(config);
+        res.write(JSON.stringify(response.data));
+        res.end();
+      } catch (error) {
+        console.error(error);
       }
-    else if (strURL.includes("info")) {
+    }
+    // Consultar el Clima de una ciudad o ubicacion especifica
+    else if (strURL.includes("clima")) {
+      var q = url.parse(req.url, true).query;
 
+      const options = {
+        method: "GET",
+        url: "https://the-weather-api.p.rapidapi.com/api/weather/" + q.ciudad,
+        headers: {
+          "x-rapidapi-key":
+            "465ae1c659msh447385a8c422d71p184958jsncee66bf33f61",
+          "x-rapidapi-host": "the-weather-api.p.rapidapi.com",
+        },
+      };
+      try {
+        const response = await axios.request(options);
+        res.write(JSON.stringify(response.data));
+        res.end();
+      } catch (error) {
+        console.error(error);
+      }
+    } 
+    // Consultar el tipo de cambio del dolar en Peru    
+    else if (strURL.includes("cambio")) {
+
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: "https://api.apis.net.pe/v2/sunat/tipo-cambio",
+        headers: {
+          Authorization:
+            "Bearer apis-token-10338.SfbnhsHRarQZGnXs0jfzw5jVPLe50XcI",
+        },
+      };
+      try {
+        const response = await axios.request(config);
+        res.write(JSON.stringify(response.data));
+        res.end();
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (strURL.includes("info")) {
       let config = {
         method: "get",
         maxBodyLength: Infinity,
@@ -44,27 +81,6 @@ http
         .catch((error) => {
           console.log(error);
         });
-    } else if (strURL.includes("clima")) {
-      var q = url.parse(req.url, true).query;
-      console.log(q.ciudad);
-      const options = {
-        method: "GET",
-        url: "https://the-weather-api.p.rapidapi.com/api/weather/" + q.ciudad,
-        headers: {
-          "x-rapidapi-key":
-            "465ae1c659msh447385a8c422d71p184958jsncee66bf33f61",
-          "x-rapidapi-host": "the-weather-api.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-        res.write(JSON.stringify(response.data));
-        res.end();
-      } catch (error) {
-        console.error(error);
-      }
     } else {
       res.write(JSON.stringify({ mensaje: "cualquier cosa" }));
       res.end();
