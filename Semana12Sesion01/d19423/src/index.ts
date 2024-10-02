287ef4de2cc13ec420ab4242d7bf341e991d8961
Roberto Pineda <rpineda@x-codec.net>
2024-10-01 20:37:21 -0500

    Clase 01102024 segunda parte

--- Semana12Sesion01/d19423/src/index.ts
+++ Semana12Sesion01/d19423/src/index.ts
@@ -31,3 +31,78 @@ const car: { type: string, model: string, year: number, kilometraje?: number } =
     year: 2009
   };
 
+
+  enum CardinalDirections {
+    North = 1,
+    East,
+    South,
+    West
+  }
+
+  let norte = CardinalDirections.North;
+  console.log(norte);
+
+  enum StatusCodes {
+    NotFound = 404,
+    Success = 200,
+    Accepted = 202,
+    BadRequest = 400
+  }
+  // logs 404
+  console.log(StatusCodes.NotFound);
+  // logs 200
+  console.log(StatusCodes.Success);
+
+
+
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+  year: CarYear,
+  type: CarType,
+  model: CarModel
+}
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const car2: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel
+};
+
+interface TV {
+    modelo: string,
+    año: number,
+    tamaño: number
+}
+
+let tvDormitorio: TV = {
+    modelo: "5tyt",
+    año: 2005,
+    tamaño :42
+}
+
+interface Rectangle {
+    height: number,
+    width: number
+  }
+  
+  interface ColoredRectangle extends Rectangle {
+    color: string
+  }
+  
+  const coloredRectangle: ColoredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red"
+  };
+
+
+  function printStatusCode(code: string | number) {
+    console.log(`My status code is ${code}.`)
+  }
+  printStatusCode(404);
+  printStatusCode('404');
