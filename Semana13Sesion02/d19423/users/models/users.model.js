dde31f728fef7622bcb9b5f32fd1556370971abf
Roberto Pineda <rpineda@x-codec.net>
2024-10-11 22:22:14 -0500

    Clase 111012024 segunda parte

--- Semana13Sesion02/d19423/users/models/users.model.js
+++ Semana13Sesion02/d19423/users/models/users.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('../../common/services/mongoose.service').mongoose;
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
     firstName: String,
     lastName: String,
@@ -12,6 +13,7 @@ const userSchema = new Schema({
 userSchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
+//
 
 userSchema.set('toJSON', {
     virtuals: true
@@ -23,7 +25,55 @@ userSchema.findById = function (cb) {
 
 const User = mongoose.model('Users', userSchema);
 
+
+exports.findByEmail = (email) => {
+    return User.find({email: email});
+};
+exports.findById = (id) => {
+    return User.findById(id)
+        .then((result) => {
+            result = result.toJSON();
+            delete result._id;
+            delete result.__v;
+            return result;
+        });
+};
+
 exports.createUser = (userData) => {
     const user = new User(userData);
     return user.save();
 };
+
+exports.list = (perPage, page) => {
+    return new Promise((resolve, reject) => {
+        User.find()
+            .limit(perPage)
+            .skip(perPage * page)
+            .exec(function (err, users) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(users);
+                }
+            })
+    });
+};
+
+exports.patchUser = (id, userData) => {
+    return User.findOneAndUpdate({
+        _id: id
+    }, userData);
+};
+
+exports.removeById = (userId) => {
+    return new Promise((resolve, reject) => {
+        User.deleteMany({_id: userId}, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(err);
+            }
+        });
+    });
+};
+
