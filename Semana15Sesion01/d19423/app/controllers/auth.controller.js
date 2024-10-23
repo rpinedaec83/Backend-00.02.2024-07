d210f4126ce9c0b8d6faa9af41d1616538647fc2
Roberto Pineda <rpineda@x-codec.net>
2024-10-22 20:43:41 -0500

    Clase 22102024 Primera Parte

---
+++ Semana15Sesion01/d19423/app/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+const db = require("../models");
+var bcrypt = require("bcryptjs");
+
+
+require( "dotenv").config();
+//const config = require("../config/auth.config");
+const User = db.user;
+const Role = db.role;
+
+
+exports.signup = (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    user.save((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        if (req.body.roles) {
+            Role.find(
+                {
+                    name: { $in: req.body.roles },
+                },
+                (err, roles) => {
+                    if (err) {
+                        res.status(500).send({ message: err });
+                        return;
+                    }
+
+                    user.roles = roles.map((role) => role._id);
+                    user.save((err) => {
+                        if (err) {
+                            res.status(500).send({ message: err });
+                            return;
+                        }
+
+                        res.send({ message: "User was registered successfully!" });
+                    });
+                }
+            );
+        } //se le crea user como rol predeterminado
+        else {
+            Role.findOne({ name: "user" }, (err, role) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                user.roles = [role._id];
+                user.save((err) => {
+                    if (err) {
+                        res.status(500).send({ message: err });
+                        return;
+                    }
+
+                    res.send({ message: "User was registered successfully!" });
+                });
+            });
+        }
+    });
+};
