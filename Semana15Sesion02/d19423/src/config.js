c211f42fce7d6ccb95e3bd7d5cadf75cd33725a8
Roberto Pineda <rpineda@x-codec.net>
2024-10-24 22:13:00 -0500

    Clase 20241024 segunda parte

--- Semana15Sesion02/d19423/src/config.js
+++ Semana15Sesion02/d19423/src/config.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT;
+
 export const SECRET = process.env.SECRET;
 export const CLIENTID = process.env.CLIENTID;
 export const CLIENTESECRET = process.env.CLIENTESECRET;
