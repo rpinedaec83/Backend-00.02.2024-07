2ce4f5bd7ecf51eedc2911407bfb067cc5bbd7fb
EJRuizM <u201620833@upc.edu.pe>
2024-11-15 23:41:02 -0500

    Hackaton14

---
+++ Hackaton14/SV74970818/src/public/socket.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+export const loadNotes = (callback) => {
+  socket.on("loadnotes", callback);
+};
+
+export const saveNote = (title, description) => {
+  socket.emit("newnote", {
+    title,
+    description,
+  });
+};
